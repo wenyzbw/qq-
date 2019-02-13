eba02c3b3a44887357c6d5401a676944abdb4a47
xyzr316 <46552865+xyzr316@users.noreply.github.com>
2019-02-13 20:12:08 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 
-// 响应force事件
+// 昵称输入框失去焦点事件函数
+function lostName(){
+    console.log("输入昵称");
+//  alert("请输入昵称后，在离开！");
+}
 
-let handlerforce = function(){
-    console.log("请输入手机号")
+// 密码输入框失去焦点事件函数
+function lostCode(){
+    console.log("输入密码");
+//  alert("请输入密码后，在离开！");
+}
+
+
+function register(){
+    console.log("注册");
 }
